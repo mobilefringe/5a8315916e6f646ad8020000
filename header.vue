6c93b4d7ee0fbf53ee6a5337457344cd6d0629f1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-08 15:46:31 -0500

    Cambridge Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -99,7 +99,7 @@
                                         <router-link tag="li" to="/pages/cambridge-advertising" class="submenu_item" exact>
                                             <a>Advertising</a>
                                         </router-link>
-                                        <router-link tag="li" to="/contact-us" class="submenu_item" exact>
+                                        <router-link tag="li" to="/pages/cambridge-contact-us" class="submenu_item" exact>
                                             <a>Contact Us</a>
                                         </router-link>
                                     </ul>
