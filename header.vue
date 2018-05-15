d32f4ffbfa992a15ede7abe4b97804663940fb59
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-15 15:32:34 -0400

    Cambridge Centre 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -31,6 +31,7 @@
                                 <label class="accessibility" for="header_newsletter">Enter Your Email</label>
                                 <input id="header_newsletter" class="newsletter_input" type="text" :placeholder='$t("header.newsletter-join")' v-model="newsletter_email"/>
                                 <router-link :to="'/newsletter?email='+ newsletter_email" >
+                                    <label class="accessibility" for="header_newsletter_submit">Submit</label>
                                     <input id="header_newsletter_submit" class="newsletter_btn" type="submit" :value='$t("header.newsletter-subscribe")' />
                                 </router-link>
                             </div>
