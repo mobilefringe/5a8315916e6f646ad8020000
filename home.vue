92d06dce1dc0fedb4e3fbcd3e8c907c31c33c9c9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-06 14:56:21 -0500

    Cambridge Ice Centre 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -64,7 +64,7 @@
     </div>
 </template>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue!vue-slick", "vue-select", "jquery", "Raphael", "mm_mapsvg", "mousewheel", "vue!svg-map"], function (Vue, Vuex, moment, tz, VueMoment, Meta, slick, VueSelect, $, Raphael, mapSvg, mousewheel, SVGMapComponent) {
+    define(["Vue", "vuex", "vue-meta", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue!vue-slick", "vue-select", "jquery", "Raphael", "mm_mapsvg", "mousewheel", "vue!svg-map"], function (Vue, Vuex, Meta, moment, tz, VueMoment, Meta, slick, VueSelect, $, Raphael, mapSvg, mousewheel, SVGMapComponent) {
         Vue.use(Meta);
         Vue.component('v-select', VueSelect.VueSelect);
         return Vue.component("home-component", {
@@ -82,6 +82,12 @@
                         infinite: true,
                         slidesToShow: 1,
                         speed: 1000
+                    },
+                    meta: {
+                        meta_title: "",
+                        meta_description: "",
+                        meta_keywords: "",
+                        meta_image: ""
                     }
                 }
             },
@@ -89,6 +95,7 @@
                 this.loadData().then(response => {
                     window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
                     this.dataLoaded = true;  
+                    this.meta = this.findMetaDataByPath(this.$route.path);
                 });
             },
             computed: {
@@ -168,16 +175,16 @@
                 }
             },
             metaInfo () {
-               return {
-                  title: this.meta.meta_title,
-                  meta: [
-                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
-                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-                  ]
-               }
+                return {
+                    title: this.meta.meta_title,
+                    meta: [
+                        { name: 'description', vmid: 'description', content: this.meta.meta_description },
+                        { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+                        { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+                        { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+                        { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+                    ]
+                }
             }
         })
     })
